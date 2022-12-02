25167fbdd9981b6f72d25a1b8c8c17c11ebd92a1
Alex <alexander.lehnbom@gmail.com>
2022-12-02 12:08:10 -0500

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,16 @@
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Developer";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Runner";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "IT Specialist";
+    }, 8000); //1s = 1000 milliseconds
+}
+
+textLoad();
+setInterval(textLoad, 12000);
